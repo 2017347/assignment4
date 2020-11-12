3a99c515ab7cd239e63fb9535c876917e2db825e
2017347 <73095384+2017347@users.noreply.github.com>
2020-11-13 00:34:49 +0500

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hellow World, How are you ?
+         ameer hamza ko corona hai
         </p>
         <a
           className="App-link"
